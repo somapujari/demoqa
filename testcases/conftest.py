40060e5de0ff4ab11d2eb539d9a7361867d9deab
somapujari <somapujari0@gmail.com>
2024-06-21 07:55:26 +0530

    reports added

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,10 +1,15 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import  Options
 
 
 @pytest.fixture
 def setup():
-    driver = webdriver.Chrome()
+    chrome_option = Options()
+    chrome_option.binary_location = r'C:\Users\Dell\AppData\Local\Google\Chrome\Application\chrome.exe'
+    service = Service(executable_path=r'C:\Users\Dell\AppData\Local\Programs\Python\Python310\chromedriver.exe')
+    driver = webdriver.Chrome(service=service, options=chrome_option)
     yield driver
 
     driver.delete_all_cookies()
